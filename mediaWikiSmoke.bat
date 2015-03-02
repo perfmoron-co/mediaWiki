af600db7f738112821d5ae7e345e178258ab3b10
Vipul Kane <kanevipul87@gmail.com>
2015-03-02 13:13:16 +1300

    adding a dumy results file

--- mediaWikiSmoke.bat
+++ mediaWikiSmoke.bat
@@ -1,1 +1,2 @@
-jmeter -n -t D:\TestScripts\script.jmx -l D:\TestScripts\scriptresults.jtl
+cd C:\apache-jmeter-2.11\apache-jmeter-2.11\bin\
+jmeter -n -t C:\MediaWiki\mediaWiki\Scripts\mediaWiki.jmx -l C:\MediaWiki\mediaWiki\Results\mediaWiki.jtl
